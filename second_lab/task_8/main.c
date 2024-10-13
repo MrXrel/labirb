bfd53bae22183fb8e8f07e8ef5061c6c9fef4233
MrXrel <artoympon169@bk.ru>
2024-10-13 21:36:15 +0300

    done

--- second_lab/task_8/main.c
+++ second_lab/task_8/main.c
@@ -4,12 +4,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define N 6
-
 void good_tests() {
-    char* sums[N];
+    char* sums[6] = {"", "", "", "", "", "", ""};
 
-    enum return_code res[N];
+    enum return_code res[6];
 
     res[0] = calculate_sum(&sums[0], 2, 2, "1101", "11");
     printf("Base 2: 1101 + 11 = %s\n", sums[0]);
@@ -18,10 +16,10 @@ void good_tests() {
     printf("Base 16: 1101 + 11 +51 + 00000C = %s\n", sums[1]);
 
     res[2] = calculate_sum(&sums[2], 5, 5, "1101", "11", "41", "000", "004");
-    printf("Base 5: 1101 + 11 + 51 + 000 + 007 = %s\n", sums[2]);
+    printf("Base 5: 1101 + 11 + 41 + 000 + 004 = %s\n", sums[2]);
 
     res[3] = calculate_sum(&sums[3], 12, 4, "A", "11", "51", "1");
-    printf("Base 8: A + 11 + 51 + 1 = %s\n", sums[3]);
+    printf("Base 12: A + 11 + 51 + 1 = %s\n", sums[3]);
 
     res[4] = calculate_sum(&sums[4], 10, 3, "1101", "11", "51");
     printf("Base 10: 1101 + 11 + 51 = %s\n", sums[4]);
@@ -29,7 +27,7 @@ void good_tests() {
     res[5] = calculate_sum(&sums[5], 12, 3, "0000", "000000", "0");
     printf("Base 12: 0000 + 000000 + 0 = %s\n", sums[5]);
     
-    for (int i = 0; i < N; ++i){
+    for (int i = 0; i < 6; ++i){
         if (res[i] == OK) {
             free(sums[i]);
         }
@@ -58,11 +56,12 @@ void print_mistakes(enum return_code error) {
 int main() { 
     // char* r;
     char* str;
-    int rt = calculate_sum(&str, 16, 0, "122", "392", "329");
+    int rt = calculate_sum(&str, 16, 3, "122", "392", "329");
     if (rt != OK) {
         print_mistakes(rt);
         return rt;
     }
     printf("Sum = %s\n", str);
     free(str);
+    good_tests();
 }
